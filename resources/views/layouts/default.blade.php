7d7a5ec617c47b4352b1b569d66db425a7208f31
Keane <jinxin0811@hotmail.com>
2020-07-26 17:28:34 +0800

    staic pages

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-  </head>
-  <body>
-    @yield('content')
-  </body>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title', 'Weibo')</title>
+</head>
+<body>
+  @yield('content')
+</body>
 </html>
